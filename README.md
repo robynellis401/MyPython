6b37f7b30a23a1f3443e47946e6077e3e2faa370
robynellis401 <robyn.ellis@sungardas.com>
2015-10-22 14:50:54 -0400

    modified:   README.md 	new file:   script0.py

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MyPython
+
+#This repo is for my personal python learning scripts
---
+++ script0.py
@@ -0,0 +1,4 @@
+#!
+#This is a simple python script that will say "Hello world" and give you the value of 2 to the 100 power.
+print('hello world')
+print(2**100)
