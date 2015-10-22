16d69892471805976fbb5446d8e7924f6578f13d
robynellis401 <robyn.ellis@sungardas.com>
2015-10-22 15:12:20 -0400

    new file:   script1.py 	new file:   script2.py

---
+++ script1.py
@@ -0,0 +1,6 @@
+#! 
+import sys #pre-defined function in python
+print(sys.platform) #gives output of platform information
+print(2**100) #2 to the 100th power
+x='spam!' #sets string value for x
+print (x*8) #prints the value for x 8 times
---
+++ script2.py
@@ -0,0 +1,10 @@
+#! 
+import sys #pre-defined function in python
+print(sys.platform) #gives output of platform information
+print(2**100) #2 to the 100th power
+x='spam!' #sets string value for x
+print (x*8) #prints the value for x 8 times
+print('Press any key to continue')
+raw_input()
+
+#with python 3 and above, raw_input() = input().  subtle change
